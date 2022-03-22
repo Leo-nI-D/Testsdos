160e67c1360fa49596728d44b3f2acabdc771ceb
Leo-nI-D <leon.levchuk@gmail.com>
2022-03-22 10:42:16 +0200

    added ip.py

---
+++ ip.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def get_ip_by_hostname():
+    hostname = input('Please enter a website address(URL): ')
+
+    try:
+        return f'Hostname: {hostname}\nIP addres: {socket.gethostbyname(hostname)}'
+    except socket.gaierror as error:
+        return f'Invalid Hostname - {error}'
+
+
+def main():
+    print(get_ip_by_hostname())
+
+    if __name__ == '__main__':
+        main()
